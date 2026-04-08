6c5497686c6a60daa88b162c1b0e97b5f4721db9
AuNguyen365 <aunguyen435@gmail.com>
2026-04-08 20:10:32 +0700

    feat: update Category and Lens

--- backend/app/Models/Lens.php
+++ backend/app/Models/Lens.php
@@ -1,4 +1,36 @@
 <?php
 
-// TODO: Implement this class
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lens extends Model
+{
+	protected $table = 'lens';
+
+	protected $fillable = [
+		'name',
+		'type',
+		'lens_type',
+		'material',
+		'index_value',
+		'coating',
+		'price',
+	];
+
+	protected $casts = [
+		'index_value' => 'decimal:2',
+		'price' => 'decimal:2',
+	];
+
+	public function cartItems()
+	{
+		return $this->hasMany(CartItem::class, 'lens_id');
+	}
+
+	public function orderItems()
+	{
+		return $this->hasMany(OrderItem::class, 'lens_id');
+	}
+}
 
